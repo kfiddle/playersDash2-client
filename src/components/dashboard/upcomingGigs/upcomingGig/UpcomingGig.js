39a496ad6802aca2c358d18e606cf48fdca3d5fc
kfiddle <kenjfiddle@gmail.com>
2023-04-26 18:16:07 -0400

    various

--- src/components/dashboard/upcomingGigs/upcomingGig/UpcomingGig.js
+++ src/components/dashboard/upcomingGigs/upcomingGig/UpcomingGig.js
@@ -1,4 +1,13 @@
-import { Card, CardContent } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import styles from "./UpcomingGig.module.css";
 
@@ -8,15 +17,38 @@ const UpcomingGig = ({ gig }) => {
   const styleOb = {
     background: "#3A4E60",
     color: "#aeaaaa",
-    margin: "0.5rem 1rem",
+    margin: "0.5rem",
+    fontWeight: "700",
+  };
+
+  const cardStyles = {
+    background: "#3A4E60",
+    color: "#aeaaaa",
+    margin: "0.25rem",
     fontWeight: "700",
   };
 
   return (
-    <Card sx={styleOb} className={styles.gigCard}>
-      <CardContent>
-        {title} {date}
-      </CardContent>
+    <Card sx={cardStyles} className={styles.gigCard}>
+      {/* <CardContent> */}
+        <Accordion sx={styleOb} className={styles.gigCard}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            {title} {date}
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        {/* // {title} {date} */}
+      {/* </CardContent> */}
     </Card>
   );
 };
