181af79217da0830f0682c983fd3d7e893fd7a04
kfiddle <kenjfiddle@gmail.com>
2023-04-29 10:51:15 -0400

    canceling testing

--- src/components/UI/CancelModal.js
+++ src/components/UI/CancelModal.js
@@ -2,10 +2,18 @@ import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
+import useGet from "../../hooks/useGet";
+
 import styles from "./CancelModal.module.css";
 
 const CancelModal = ({ handleCloser }) => {
+  const getter = useGet();
+
   const handleClose = () => handleCloser();
+
+  const handleCancel = async () => {
+    const testing = await getter(`players/clear-row`);
+  };
 
   return (
     <Modal show={true} fullscreen={true} onHide={handleClose}>
@@ -14,10 +22,11 @@ const CancelModal = ({ handleCloser }) => {
       </Modal.Header>
       <Modal.Body>
         Are you sure you must cancel your participation in this gig? Clicking
-        CANCEL below will notify the manager of your decision, and you will be replaced.
+        CANCEL below will notify the manager of your decision, and you will be
+        replaced.
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="primary" onClick={handleClose}>
+        <Button variant="primary" onClick={handleCancel}>
           <h5>CANCEL</h5>
         </Button>
         <Button variant="secondary" onClick={handleClose}>
